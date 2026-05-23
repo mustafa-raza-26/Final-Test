32294c397362e9b6e2be06d93bfda46efcc60962
mustafa-raza-26 <mustafa778823@gmail.com>
2026-05-23 20:12:05 +0500

    Update

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const supabaseURL = 'https://escflpzgeayghspqdkcr.supabase.co/appoinmentForm';
-const supabaseKey = 'sb_publishable_X3l_oYzwfdLDEfOVXIwB2w_wcYfCBn2';
+const supabaseURL = 'https://escflpzgeayghspqdkcr.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVzY2ZscHpnZWF5Z2hzcHFka2NyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg3NTI3MDUsImV4cCI6MjA5NDMyODcwNX0.Uw7p0b9KtNSxX-vf3_pJQ0ZmstXJbYtZR98x0RzNDKk';
 const client = supabase.createClient(supabaseURL,supabaseKey);
+console.log(client);
 
 let book = document.getElementById("book");
 if(book){
@@ -10,8 +11,7 @@ if(book){
     if (!session) {
         alert('Sign up For Booking Appoinment');
     }else{
-        // window.location.href = 'https://mustafa-raza-26.github.io/Final-Test/form.html'
-        window.location.href = '/form.html'
+        window.location.href = 'https://mustafa-raza-26.github.io/Final-Test/form.html'
     }
     });
 })
