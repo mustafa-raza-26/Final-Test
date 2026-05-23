5f482f0321ca2aed3bbaf7b01e116a2bf38b677b
mustafa-raza-26 <mustafa778823@gmail.com>
2026-05-23 14:40:30 +0500

    Update

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const supabaseURL = 'https://qqgjdalywbvibvvgwvkf.supabase.co';
-const supabaseKey = 'sb_publishable_9RlJxkVD9-n85tF-MOVxwg_gvtMxtRv';
+const supabaseURL = 'https://escflpzgeayghspqdkcr.supabase.co/appoinmentForm';
+const supabaseKey = 'sb_publishable_X3l_oYzwfdLDEfOVXIwB2w_wcYfCBn2';
 const client = supabase.createClient(supabaseURL,supabaseKey);
 
 let book = document.getElementById("book");
