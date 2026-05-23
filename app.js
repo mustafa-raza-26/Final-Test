b47499c2776363a6da9de8fefa4d81c84b095577
mustafa-raza-26 <mustafa778823@gmail.com>
2026-05-23 20:18:15 +0500

    Update 2

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const supabaseURL = 'https://escflpzgeayghspqdkcr.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVzY2ZscHpnZWF5Z2hzcHFka2NyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg3NTI3MDUsImV4cCI6MjA5NDMyODcwNX0.Uw7p0b9KtNSxX-vf3_pJQ0ZmstXJbYtZR98x0RzNDKk';
+const supabaseKey = 'sb_publishable_X3l_oYzwfdLDEfOVXIwB2w_wcYfCBn2';
 const client = supabase.createClient(supabaseURL,supabaseKey);
 console.log(client);
 
